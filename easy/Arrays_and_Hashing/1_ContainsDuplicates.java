e62b0e778828ed9ae18cf46283babea4f1690377
nandit27 <nanditkalaria27@gmail.com>
2025-05-11 20:45:33 +0530

    Added: ContainsDuplicates

---
+++ easy/Arrays_and_Hashing/1_ContainsDuplicates.java
@@ -0,0 +1,12 @@
+import java.util.*;
+ 
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            set.add(nums[i]);
+        }
+        if(set.size() == nums.length) return false;
+        else return true;
+    }
+}
