ab34f2a37ced3001fba1a40e1eaa6f4b3aa0aeaa
nandit27 <nanditkalaria27@gmail.com>
2025-05-11 20:49:00 +0530

    Added: TwoSum

---
+++ easy/Arrays_and_Hashing/3_TwoSum.java
@@ -0,0 +1,17 @@
+import java.util.*;
+ 
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] ans = new int[2];
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(target-nums[i])){
+                ans[1] = i;
+                ans[0] = map.get(target-nums[i]);
+                break;
+            }
+            map.put(nums[i],i);
+        }
+        return ans;
+    }
+}
