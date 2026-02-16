ba625cfef458d2f9a841d225df1dc38516b92a79
nandit27 <nanditkalaria27@gmail.com>
2026-02-16 20:51:04 +0530

    Added: Unbounded Knapsack: Coin Change (Min No. of Choices)

---
+++ medium/DP/11_Unbounded_Knapsack:_Coin_Change_(Min_No._of_Choices).java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class HelloWorld {
+    //Coin Change - Min. No. of Choices
+    public static void main(String[] args){
+        int target = 4;
+        int[] coin = {1,2,3};
+        int N = coin.length;
+
+        int ans = coinChange(coin,N,target);
+        System.out.println(ans);
+    }
+    public static int coinChange(int[] coin, int N, int target){
+        int[][] t = new int[N+1][target+1];
+        for(int i=0;i<=N;i++){
+            t[i][0] = 0;
+        }
+        for(int j=1;j<=target;j++) {
+            t[0][j] = Integer.MAX_VALUE - 1;
+        }
+        for(int i=1;i<=N;i++) {
+            for (int j = 1; j <= target; j++) {
+                if (coin[i - 1] <= j) {
+                    t[i][j] = Math.min(t[i-1][j], 1 + t[i][j - coin[i-1]]);
+                } else {
+                    t[i][j] = t[i - 1][j];
+                }
+            }
+        }
+        return t[N][target];
+    }
+}
